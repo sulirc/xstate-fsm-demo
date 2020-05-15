d131de3bbd5c153bced6435e0720bf124b2e817d
yanguangjie <yanguangjie@bytedance.com>
2020-05-15 11:51:38 +0800

    chore: remove test files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// import fsm from '../packages/fsm';
+
+// console.log(fsm);
+
 ReactDOM.render(
   <App />, document.getElementById('root')
 );
